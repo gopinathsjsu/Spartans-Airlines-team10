b8048a06727c6fac511b8fd5d1d91c4f532184e8
Saurabh Vijaywargia <saurabh.vijaywargia@sjsu.edu>
2021-11-05 15:29:31 -0700

    Corrected the button margin in Profile page

---
+++ Front-End/Navigationbar.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+// import '../../App.css';
+import { Link } from 'react-router-dom';
+import './Navigationbar.css'
+import {
+    Navbar, Nav, Button,
+} from 'react-bootstrap';
+
+class Navigationbar extends Component {
+
+
+    render() {
+        const navLogin = (
+            <Nav className="ml-auto">
+
+                <Link
+                    id="loginLink"
+                    to={{
+                        pathname: '/login',
+                    }}
+                ><Button id="navbarchart" className="mr-sm-2 navbarbuttons">Login
+                    </Button></Link>
+
+                <Link
+                    id="signUpLink"
+                    to={{
+                        pathname: '/signup',
+                    }}
+                ><Button id="navbarchart" className="navbarbuttons">Sign Up
+                    </Button></Link>
+				<Link
+                    id="profilepageLink"
+                    to={{
+                        pathname: '/profile',
+                    }}
+                >
+                    <Button id="navbarchart" className="navbarbuttons">
+                        Profile
+                    </Button>
+                </Link>
+            </Nav>
+        );
+        return (
+            <div>
+                <Navbar id="nav-bar">
+                    <div className="container">
+                        <Navbar.Brand id="nav-brand">
+                            <img
+                                alt=""
+                                src={`${window.location.origin}/san-jose-state-university-icon.png`}
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                            />
+                            {' '}
+                            <Link
+                                id="landingPageLink"
+                                to={{
+                                    pathname: '/',
+                                }}
+                            >
+                            Spartan Airlines
+                            </Link>
+
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        {navLogin}
+                    </div>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+
+export default Navigationbar;
---
+++ Front-End/ProfilePage.js
@@ -0,0 +1,77 @@
+import {
+    Form, Button, Container, Col, Row,
+} from 'react-bootstrap';
+import Navigationbar from '../Navigationbar/Navigationbar'
+import { useDispatch } from 'react-redux'
+import { mainSliceActions } from '../../store/mainSlice'
+import './ProfilePage.css'
+
+const ProfilePage = () => {
+    const dispatch = useDispatch()
+
+    const onChangeFullName = (e) => {
+        dispatch(mainSliceActions.setFullName(e.target.value))
+    }
+    const onChangeEmailAddress = (e) => {
+        dispatch(mainSliceActions.setEmailAddress(e.target.value))
+    }
+	    const onChangeAddress = (e) => {
+        dispatch(mainSliceActions.setAddress(e.target.value))
+    }
+	    const onChangePhoneNumber = (e) => {
+        dispatch(mainSliceActions.setPhoneNumber(e.target.value))
+    }
+    const onChangeChangePassword = (e) => {
+        dispatch(mainSliceActions.setChangePassword(e.target.value))
+    }
+    return (
+        <div>
+            <Navigationbar />
+            <div className="container">
+                <Container>
+                    <div>
+                        <Row>
+                            <Col>
+                                <Form id="profile-form" method="post">
+                                    <h1>Profile</h1>
+                                    <p>Enter Details</p>
+                                    <Form.Group className="profilebox" controlId="formFullName">
+                                        <Form.Label>Full Name</Form.Label>
+                                        <Form.Control type="text" name="fullname" placeholder="Enter Your Full Name" onChange={onChangeFullName} required />
+                                    </Form.Group>
+                                    <Form.Group className="profilebox" controlId="formEmailAddress">
+                                        <Form.Label>Email Address</Form.Label>
+                                        <Form.Control type="email" name="emailaddress" placeholder="Enter Your Email Address" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" onChange={onChangeEmailAddress} required />
+                                    </Form.Group>
+  									<Form.Group className="profilebox" controlId="formAddress">
+                                        <Form.Label>Address</Form.Label>
+                                        <Form.Control type="text" name="address" placeholder="Enter Your Address" onChange={onChangeAddress} required />
+                                    </Form.Group>
+									<Form.Group className="profilebox" controlId="formPhoneNumber">
+                                        <Form.Label>Phone Number</Form.Label>
+                                        <Form.Control type="text" name="phonenumber" placeholder="Enter Your Phone Number" onChange={onChangePhoneNumber} required />
+                                    </Form.Group>
+									<Form.Group className="profilebox" controlId="formChangePassword">
+                                        <Form.Label>Change Password</Form.Label>
+                                        <Form.Control type="password" name="changepassword" placeholder="Enter Your Password to Change" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" onChange={onChangeChangePassword} required />
+                                    </Form.Group>
+                                    <small>
+                                        Must contain at least one  number and one uppercase and
+                                        lowercase letter, and at least 8 or more characters
+									</small>
+                                    <br />
+                                    <br />
+                                    <Button id="profilebutton" type="submit">
+                                        Submit
+                      </Button>
+                                </Form>
+                            </Col>
+                        </Row>
+                    </div>
+                </Container>
+            </div>
+        </div>
+    )
+}
+
+export default ProfilePage;
