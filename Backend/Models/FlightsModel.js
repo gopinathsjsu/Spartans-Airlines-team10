ceb743b6c84bf63f6e32f52b75500b08102bccd3
shravanvallaban <kuppu.shravan@gmail.com>
2021-11-09 18:03:26 -0800

    added addFlights API

---
+++ Backend/Models/FlightsModel.js
@@ -0,0 +1,90 @@
+const mongoose = require("mongoose");
+
+const flightSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  flightNumber: {
+    type: String,
+    required: true,
+    maxlength: 10,
+  },
+  origin: {
+    type: String,
+    required: true,
+    maxlength: 30,
+  },
+  originCode: {
+    type: String,
+    required: true,
+    maxlength: 5,
+  },
+  destination: {
+    type: String,
+    required: true,
+    maxlength: 30,
+  },
+  destinationCode: {
+    type: String,
+    required: true,
+    maxlength: 5,
+  },
+  departureDate: {
+    type: Date,
+    required: true,
+  },
+  arrivalDate: {
+    type: Date,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  capacity: {
+    type: Number,
+    required: true,
+    max: [1000, 'The capacity cant exceed 1000'],
+  },
+  seatsLeft: {
+    type: Number,
+    required: true,
+  },
+  travelDistance: {
+    type: Number,
+    required: true,
+    min:[100, 'distance should be a minimum of 100km'],
+    max:[20000,'distance cannot exceed 20,000km']
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  mileagePoints: {
+    type: Number,
+    required: true,
+    min: [100,'minimum points cannot be less than 100'],
+    max: [1000,'maximum points cannot be more than 1000'],
+  },
+  seats: [
+    {
+      seatID: {
+        type: String,
+        required: true,
+        maxlength: 5,
+      },
+      seatNumber: {
+        type: Number,
+        required: true,
+        maxlength: 3,
+      },
+      status: {
+        type: String,
+        required: true,
+        maxlength: 2,
+      },
+    },
+  ],
+});
+
+var flightModel = mongoose.model("flights", flightSchema);
+
+module.exports = flightModel;
--- Backend/Routes/employee.js
+++ Backend/Routes/employee.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 const Employee = require("../Models/employee");
+const Flight = require("../Models/FlightsModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
@@ -94,6 +95,165 @@ router.post("/login", (req, res, next) => {
       res.status(500).json({
         error: err,
       });
+    });
+});
+
+router.post("/addFlight", (req, res, next) => {
+  depDate = req.body.depDate;
+  depMonth = req.body.depMonth;
+  depYear = req.body.depYear;
+  depHrs = req.body.depHrs;
+  depMins = req.body.depMins;
+  secs = "00";
+
+  var depdate = new Date(
+    Date.UTC(depYear, depMonth, depDate, depHrs, depMins, secs)
+  );
+  arrDate = req.body.arrDate;
+  arrMonth = req.body.arrMonth;
+  arrYear = req.body.arrYear;
+  arrHrs = req.body.arrHrs;
+  arrMins = req.body.arrMins;
+  var arrdate = new Date(
+    Date.UTC(arrYear, arrMonth, arrDate, arrHrs, arrMins, secs)
+  );
+  if (arrdate < depdate || arrdate == depdate) {
+    return res.status(409).json({
+      message: "Arrival time cannot be equal to or before the Departure time",
+    });
+  }
+
+  Flight.find({
+    flightNumber: req.body.flightNumber,
+    arrivalDate: {$gte: depdate.toISOString()},
+    depDate: {$lte: depdate.toISOString()}
+  })
+    .exec()
+    .then((flight) => {
+      if (flight.length >= 1) {
+        console.log(flight);
+
+        return res.status(409).json({
+          message:
+            "Flight exists in the records already or there is a time conflict",
+        });
+      } else {
+        flightNumber = req.body.flightNumber;
+        carrier = req.body.carrier;
+        origin = req.body.origin;
+        originCode = req.body.originCode;
+        destination = req.body.destination;
+        destinationCode = req.body.destinationCode;
+        depISOString = depdate.toISOString();
+        console.log("departure date" + depISOString);
+
+        var arrdate = new Date(
+          Date.UTC(arrYear, arrMonth, arrDate, arrHrs, arrMins, secs)
+        );
+        console.log("arrival date" + arrdate);
+
+        arrISOString = arrdate.toISOString();
+        console.log("arrival date" + arrISOString);
+        price = req.body.price;
+        capacity = req.body.capacity;
+
+        travelDistance = req.body.travelDistance;
+
+        const diffInMilliseconds = Math.abs(arrdate - depdate);
+
+        const hours = Math.floor(diffInMilliseconds / 3600) % 24;
+        // diffInMilliSeconds -= hours * 3600;
+        console.log("calculated hours", hours);
+
+        function timeDiffCalc(dateFuture, dateNow) {
+          let diffInMilliSeconds = Math.abs(dateFuture - dateNow) / 1000;
+
+          // calculate days
+          const days = Math.floor(diffInMilliSeconds / 86400);
+          diffInMilliSeconds -= days * 86400;
+          console.log("calculated days", days);
+
+          // calculate hours
+          const hours = Math.floor(diffInMilliSeconds / 3600) % 24;
+          diffInMilliSeconds -= hours * 3600;
+          console.log("calculated hours", hours);
+
+          // calculate minutes
+          const minutes = Math.floor(diffInMilliSeconds / 60) % 60;
+          diffInMilliSeconds -= minutes * 60;
+          console.log("minutes", minutes);
+
+          let difference = "";
+          if (days > 0) {
+            difference += days === 1 ? `${days} day, ` : `${days} days, `;
+          }
+
+          difference +=
+            hours === 0 || hours === 1 ? `${hours} hour, ` : `${hours} hours, `;
+
+          difference +=
+            minutes === 0 || hours === 1
+              ? `${minutes} minutes`
+              : `${minutes} minutes`;
+
+          return difference;
+        }
+
+        timeDiff = timeDiffCalc(arrdate, depdate);
+        duration = timeDiff;
+        mileagePoints = req.body.mileagePoints;
+
+        rows = Math.ceil(capacity / 6);
+
+        const alpha = Array.from(Array(rows)).map((e, i) => i + 65);
+        const alphabet = alpha.map((x) => String.fromCharCode(x));
+
+        var seats = [];
+        for (i = 1; i <= rows; i++) {
+          for (j = 1; j <= 6; j++) {
+            obj = {
+              seatID: alphabet[j],
+              seatNumber: j,
+              status: "A",
+            };
+            seats.push(obj);
+          }
+        }
+
+        // console.log(seats);
+
+        const flight = new Flight({
+          _id: new mongoose.Types.ObjectId(),
+          flightNumber: flightNumber,
+          origin: origin,
+          originCode: originCode,
+          destination: destination,
+          destinationCode: destinationCode,
+          departureDate: depISOString,
+          arrivalDate: arrISOString,
+          capacity: capacity,
+          seatsLeft: capacity,
+          travelDistance: travelDistance,
+          duration: duration,
+          mileagePoints: mileagePoints,
+          seats: seats,
+          price: price,
+        });
+        flight
+          .save()
+          .then((result) => {
+            console.log(result);
+            res.status(201).json({
+              message: "Flight Added",
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+              error: err,
+            });
+          });
+      }
     });
 });
 
