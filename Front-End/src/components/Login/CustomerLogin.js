08202a19eee49161d51392cc60661d536528f5a7
Shakthivel <rn.shakthivel@gmail.com>
2021-11-10 16:52:30 -0800

    Integration: Login Front End and Backend API Integrated

--- Front-End/src/components/Login/CustomerLogin.js
+++ Front-End/src/components/Login/CustomerLogin.js
@@ -1,13 +1,17 @@
 import {
     Form, Button, Container, Col, Row, Figure
 } from 'react-bootstrap';
+import toast, { Toaster } from 'react-hot-toast';
 import Navigationbar from '../Navigationbar/Navigationbar'
-import { useDispatch } from 'react-redux'
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux'
 import { mainSliceActions } from '../../store/mainSlice'
 import './CommonLogin.css'
 
 const CustomerLogin = () => {
     const dispatch = useDispatch()
+    const email = useSelector(state => state.mainSlice.email)
+    const password = useSelector(state => state.mainSlice.password)
 
     const onChangeEmail = (e) => {
         dispatch(mainSliceActions.setEmail(e.target.value))
@@ -17,8 +21,40 @@ const CustomerLogin = () => {
         dispatch(mainSliceActions.setPassword(e.target.value))
     }
 
+    const onLogin = (data) => {
+        sessionStorage.setItem('userId', data._id)
+        dispatch(mainSliceActions.setFirstName(data.firstName))
+        dispatch(mainSliceActions.setLastName(data.lastName))
+        dispatch(mainSliceActions.setEmail(data.emailID))
+        dispatch(mainSliceActions.setAddress(data.address))
+        dispatch(mainSliceActions.setPhoneNumber(data.phoneNum))
+        dispatch(mainSliceActions.setGender(data.gender))
+        dispatch(mainSliceActions.setPassword(''))
+    }
+
+    const handleLogin = (event) => {
+        event.preventDefault()
+        const data = {
+            emailID: email,
+            password
+        }
+        axios.defaults.withCredentials = true;
+        axios.post('http://localhost:3001/login', data)
+            .then((response) => {
+                onLogin(response.data)
+                validLogin()
+            })
+            .catch(() => {
+                invalidLogin()
+            })
+    }
+
+    const validLogin = () => toast.success('Logged In Successfully!')
+    const invalidLogin = () => toast.error('Invalid Email Address or Password.')
+
     return (
         <div>
+            <Toaster />
             <Navigationbar />
             <div className="container">
                 <Container>
@@ -30,7 +66,7 @@ const CustomerLogin = () => {
                                 />
                             </Col>
                             <Col>
-                                <Form id="login-form" method="post">
+                                <Form id="login-form" method="post" onSubmit={handleLogin}>
                                     <h1>Customer Login</h1>
                                     <p>Enter your details to login</p>
                                     <Form.Group className="loginbox" controlId="formUsername">
@@ -42,7 +78,7 @@ const CustomerLogin = () => {
                                     <br />
                                     <Button id="loginbutton" type="submit">
                                         Submit
-                      </Button>
+                                    </Button>
                                 </Form>
                             </Col>
                         </Row>
