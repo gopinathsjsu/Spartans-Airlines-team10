6b78b0ba98b0fd6f66781b5577993cfecc32dbc5
shravanvallaban <kuppu.shravan@gmail.com>
2021-11-10 18:15:28 -0800

    changed the routing file structure for employees to make it easy for Frontend devs

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,12 @@
 //  import the require dependencies
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const config = require('./Util/config');
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./Util/config");
 
 // use cors to allow cross origin resource sharing
 app.use(cors({ origin: `${config.api_local}`, credentials: true }));
@@ -19,26 +19,26 @@ app.use(
     saveUninitialized: false,
     duration: 60 * 60 * 1000, // Overall duration of Session : 30 minutes : 1800 seconds
     activeDuration: 5 * 60 * 1000,
-  }),
+  })
 );
 
 app.use(bodyParser.json());
-app.use(express.urlencoded({extended: true})); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Allow Access Control
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', `${config.api_local}`);
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader("Access-Control-Allow-Origin", `${config.api_local}`);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,HEAD,OPTIONS,POST,PUT,DELETE',
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
   );
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
-  res.setHeader('Cache-Control', 'no-cacbodyhe');
+  res.setHeader("Cache-Control", "no-cacbodyhe");
   next();
 });
 
@@ -49,28 +49,27 @@ const options = {
 console.log(config.mongoURI);
 mongoose.connect(config.mongoURI, options, (err, res) => {
   if (err) {
-    console.log('cannot connect to mongo db');
+    console.log("cannot connect to mongo db");
   } else {
     // console.log(res);
-    console.log('connection successful');
+    console.log("connection successful");
   }
 });
 mongoose.set("debug", (collectionName, method, query, doc) => {
   console.log(`${collectionName}.${method}`, JSON.stringify(query), doc);
 });
 // Routing
-const login = require('./Routes/Login.js');
-const signUp = require('./Routes/Signup.js');
-const profile = require('./Routes/Profile.js');
-const empRoutes = require("./Routes/employee");
-
+const login = require("./Routes/Login.js");
+const signUp = require("./Routes/Signup.js");
+const profile = require("./Routes/Profile.js");
+const addFlightRoutes = require("./Routes/Employee/addFlight");
 
 // Route config
-app.use('/login', login);
-app.use('/signup', signUp);
-app.use('/profile', profile);
-app.use("/employee", empRoutes);
-
+app.use("/login", login);
+app.use("/signup", signUp);
+app.use("/profile", profile);
+app.use("/employee/addFlight", addFlightRoutes);
+// app.use("/employee/edi")
 
 app.use((req, res, next) => {
   const error = new Error("Route Not found");
@@ -87,7 +86,6 @@ app.use((error, req, res, next) => {
   });
 });
 
-
 // start your server on port 3001
 app.listen(config.serverPort);
-console.log('Server Listening on port 3001');
+console.log("Server Listening on port 3001");
